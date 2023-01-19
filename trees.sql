37e3d8d5390a85e45de92c0774a421caf79a8792
Дмитрий Мохирев <dvmohirev@gmail.com>
2023-01-20 01:29:11 +0500

    dump db
    Используйте этот файл для создания БД, чтобы программа работала на сервере, а не через консоль.
    Чтобы запустить через консоль - используйте файл main.php

---
+++ trees.sql
@@ -0,0 +1,50 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Хост: 127.0.0.1:3306
+-- Время создания: Янв 19 2023 г., 22:27
+-- Версия сервера: 5.6.51
+-- Версия PHP: 7.4.27
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- База данных: `fruitgarden`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `trees`
+--
+
+CREATE TABLE `trees` (
+  `tree_id` int(8) NOT NULL,
+  `type_tree` varchar(30) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `min_thing` int(5) NOT NULL,
+  `max_thing` int(5) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Индексы сохранённых таблиц
+--
+
+--
+-- Индексы таблицы `trees`
+--
+ALTER TABLE `trees`
+  ADD PRIMARY KEY (`tree_id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
